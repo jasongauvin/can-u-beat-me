438499ff6258435ba5f3c050d007b42a8c93496a
ShAdOwInK <alexandredelaloydev@gmail.com>
2018-11-20 17:48:35 +0100

    🚧 wi^p

--- src/js/ui.js
+++ src/js/ui.js
@@ -1,61 +1,66 @@
 if (typeof (Storage) !== 'undefined') {
-	let theProgressBars = [34, 100, 67];
-	let theExBoolsEasy = [true, false, true];
-	let theExBoolsMedium = [false, true, false];
-	let theExBoolsHard = [true, false, false];
-
+	let theProgressBars = [0, 100, 67];
+	let theExBools = [
+		[true, true, true],
+		[false, true, false],
+		[true, false, false],
+	];
 	const setLocalStorage = () => {
 		localStorage.notEmpty = true;
 		localStorage.progressBars = JSON.stringify(theProgressBars);
-		localStorage.exBoolsEasy = JSON.stringify(theExBoolsEasy);
-		localStorage.exBoolsMedium = JSON.stringify(theExBoolsMedium);
-		localStorage.exBoolsHard = JSON.stringify(theExBoolsHard);
+		localStorage.exBoolsEasy = JSON.stringify(theExBools[0]);
+		localStorage.exBoolsMedium = JSON.stringify(theExBools[1]);
+		localStorage.exBoolsHard = JSON.stringify(theExBools[2]);
 		return console.log('📤 | localStorage set !');
 	};
 	const getLocalStorage = () => {
 		theProgressBars = JSON.parse(localStorage.progressBars);
-		theExBoolsEasy = JSON.parse(localStorage.exBoolsEasy);
-		theExBoolsMedium = JSON.parse(localStorage.exBoolsMedium);
-		theExBoolsHard = JSON.parse(localStorage.exBoolsHard);
+		theExBools[0] = JSON.parse(localStorage.exBoolsEasy);
+		theExBools[1] = JSON.parse(localStorage.exBoolsMedium);
+		theExBools[2] = JSON.parse(localStorage.exBoolsHard);
 		return console.log('📥 | localStorage get !');
 	};
 
-	const updatePgBar = (name, myExBools, myProgressBar, DOMprogressBar, DOMspan, DOMexs) => {
+	const updatePgBar = (name, index, DOMprogressBar, DOMspan, DOMexs) => {
 		// I - get localStorage and set all settings
 
+		// get localStorage
 		getLocalStorage();
-		console.log(localStorage);
-		console.log(theProgressBars);
-		console.log(theExBoolsEasy);
-		console.log(theExBoolsMedium);
-		console.log(theExBoolsHard);
 		// set the progress bar
-		console.log(myProgressBar);
-		DOMprogressBar.style.width = `${theProgressBars[0]}%`;
+		DOMprogressBar.style.width = `${theProgressBars[index]}%`;
 		// set the span %
-		DOMspan.innerHTML = `${name}: ${theProgressBars[0]}%`;
+		DOMspan.innerHTML = `${name}: ${theProgressBars[index]}%`;
 
 		// II - loop all the buttons of a difficulty
 
 		// for all the bool button of the ex
-		for (let i = 0; i < myExBools.length; i++) {
+		for (let i = 0; i < 3; i++) {
 			// for every verification button I click
 			DOMexs[i].addEventListener('click', () => {
 				// if the current bool is true
-				if (myExBools[i]) {
-					console.log('Congrats !');
-					console.log(theProgressBars[0]);
+				console.log(theExBools[index][i]);
+				if (theExBools[index][i]) {
+					console.log('✅ | True !');
 					// update the progress bar percent
-					const newProgressBar = theProgressBars[0] + 33;
+					const newProgressBar = theProgressBars[index] + 33;
 					// update the progress bar with new progress bar %
 					DOMprogressBar.style.width = `${newProgressBar}%`;
 					// set the span % with new progress bar %
 					DOMspan.innerHTML = `${name}: ${newProgressBar}%`;
 
-					theProgressBars[0] = newProgressBar;
-					console.log(theProgressBars);
+					// III - update the current changes
+
+					// in local first
+					theProgressBars[index] = newProgressBar;
+					theExBools[index][i] = true;
+					// in the localStorage second
 					setLocalStorage();
-					console.log(localStorage);
+				}
+				else {
+					console.log('❌ | False !');
+					theExBools[index][isFinite] = false;
+					// in the localStorage second
+					setLocalStorage();
 				}
 			});
 		}
@@ -67,8 +72,7 @@ if (typeof (Storage) !== 'undefined') {
 
 	const Easy = {
 		'name': 'easy',
-		'myExBools': theExBoolsEasy,
-		'myProgressBar': theProgressBars[0],
+		'index': 0,
 		'DOMprogressBar': document.querySelector('#easyPgBar .progressBars__progressBarBox__parent__child'),
 		'DOMspan': document.querySelector('#easyPgBar span'),
 		'DOMexs': document.querySelectorAll('#Easy .verification__button'),
